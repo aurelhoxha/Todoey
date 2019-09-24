9de0c25c8a9a47d43bbfb7fbd586db3bdbc274b7
Aurel Hoxha <aureli.hoxha@gmail.com>
2019-09-24 16:28:10 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Aurel Hoxha on 9/24/19.
+//  Copyright © 2019 Ritech Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
